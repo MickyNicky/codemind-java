31c3c273ecbfe74d81b1f267b6ec325a9f3c4e82
MickyNicky <156397143+MickyNicky@users.noreply.github.com>
2024-01-12 10:17:46 +0530

    import java.util.Scanner; public class Taps {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int X=sc.nextInt();         int Y=sc.nextInt();         int Z=((X*Y)/(X+Y));         System.out.println(Z);     } }

--- Water_Taps.java
+++ Water_Taps.java
@@ -1,12 +1,12 @@
- import java.util.Scanner;
- public class Taps
+import java.util.Scanner;
+public class Taps
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
         int X=sc.nextInt();
         int Y=sc.nextInt();
-        int c=((X*Y)/(X+Y));
-    System.out.print(c);
+        int Z=((X*Y)/(X+Y));
+        System.out.println(Z);
     }
 }
