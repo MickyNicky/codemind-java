84598e4866692bada92432047fb04710acab0f4e
MickyNicky <156397143+MickyNicky@users.noreply.github.com>
2024-02-15 09:42:40 +0530

    import java.util.*; public class CodeMind {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int X=sc.nextInt();         if(X>=40)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Subscription.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class CodeMind
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        if(X>=40)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
