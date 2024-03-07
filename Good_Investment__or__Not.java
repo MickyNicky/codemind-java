2b3927b53c6ba1b08dd2b57ad7a54274206c3b81
MickyNicky <156397143+MickyNicky@users.noreply.github.com>
2024-03-07 19:28:23 +0530

    import java.util.Scanner;
    public class Investment {
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
    
            // Input
            int X = scanner.nextInt(); // Interest rate
            int Y = scanner.nextInt(); // Inflation rate
    
            // Output
            System.out.println(isInvestmentGood(X, Y));
            
            scanner.close();
        }
    
        public static String isInvestmentGood(int X, int Y) {
            if (X >= 2 * Y) {
                return "YES";
            } else {
                return "NO";
            }
        }
    }

--- Good_Investment__or__Not.java
+++ Good_Investment__or__Not.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Investment {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int X = scanner.nextInt();
-        int Y = scanner.nextInt();
+
+        // Input
+        int X = scanner.nextInt(); // Interest rate
+        int Y = scanner.nextInt(); // Inflation rate
+
+        // Output
         System.out.println(isInvestmentGood(X, Y));
+        
         scanner.close();
     }
+
     public static String isInvestmentGood(int X, int Y) {
         if (X >= 2 * Y) {
             return "YES";
