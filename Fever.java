b184216cee88705bc4522e4f839d56e6fabe9450
MickyNicky <156397143+MickyNicky@users.noreply.github.com>
2024-03-08 07:56:16 +0530

    import java.util.Scanner;
    public class Main {
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            int X = scanner.nextInt();
            if (X>98) {
                System.out.println("YES");
            } else {
                System.out.println("NO");
            }
        }
    }

---
+++ Fever.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        if (X>98) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
